f7f78619d6f498d6328f4a3eca974655753d4642
Agustin Larrarte <a.larrarte@gmail.com>
2020-08-11 08:41:55 -0300

    test commit

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Configure the AWS Provider -
+# Configure the AWS Provider - -
 # to connect to an aws account. (https://www.terraform.io/docs/providers/aws/index.html)
 # $ export AWS_ACCESS_KEY_ID="anaccesskey"
 # $ export AWS_SECRET_ACCESS_KEY="asecretkey"
